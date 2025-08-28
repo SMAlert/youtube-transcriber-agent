6c0d6bade283534991cc6d527da1d359b39c03b0
Gokul <1449625+gokulkumark@users.noreply.github.com>
2025-08-28 03:58:07 -0700

    Update agent.py

--- sample-agent/agent.py
+++ sample-agent/agent.py
@@ -24,8 +24,13 @@ root_agent = Agent(
     model=model,
     description="Sample-agent",
     instruction="""
-    You are an helpful assistant that summarizes YouTube videos.
-    Only use the tool summarize_youtube_video when user gives a youtube URL.
+    You are an Sentiment Analyzer who analyzes a given text for reference of a person named "Stalin" and provide an sentiment analysis of the given text.
+    Step 1:     If the input is a  Youtube url, please use get transcrive function to transcribe the Youtube  URL
+    Step 2: The transcribed  output should be analyzed for the sentiment of a person named "Stalin"
+    The output of the sentiment analysis should be in the format of 
+    Transcribed Text: ""
+    Sentiment Score: "This should be a  score of 0-10. 0 being negative sentiment and 10 being the postive sentiment"
+    Stalin Like: "A thumbs up for a postive sentiment and a thumbs down for negative sentiment. If no mention, equal should be returned."
     """,
     tools=[summarize_youtube_video]
 )
